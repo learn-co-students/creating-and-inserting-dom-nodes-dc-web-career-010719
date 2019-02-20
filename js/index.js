48a23c2cb6575bfb829422bfa833d77a49787c89
dcjones1 <dcejjones@gmail.com>
2019-02-20 16:44:34 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,24 @@
-//this file is optional
-//you can use this to paste in js code and have it run when index.html is loaded
+// create a new element and style it
+let element = document.createElement('div')
+element.innerHTML = 'Hello, DOM!'
+element.style.backgroundColor = '#f9f9f9'
+
+// assign it a parent and more styling
+document.body.appendChild(element)
+element.style.textAlign = 'center'
+
+// new elements and assignment to parents
+let ul = document.createElement('ul')
+for (let i = 0; i < 3; i++) {
+  let li = document.createElement('li')
+  li.innerHTML = (i + 1).toString()
+  ul.appendChild(li)
+}
+element.appendChild(ul)
+ul.style.textAlign = 'left'
+
+// if I wanted to remove anything
+// remove one item
+ul.removeChild(ul.querySelector('li:nth-child(2)'))
+// remove whole list, this is a METHOD
+ul.remove() // <--- remember the parentheses
